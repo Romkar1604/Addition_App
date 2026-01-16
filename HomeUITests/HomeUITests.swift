5b274bfb6adf0eda87fd3369b32aa15ee742b6f9
Omkar Ranawade <omkar.ranawade98@gmail.com>
2026-01-16 09:42:31 -0500

    Initial Commit

---
+++ HomeUITests/HomeUITests.swift
@@ -0,0 +1,17 @@
+//
+//  HomeUITests.swift
+//  HomeUITests
+//
+//  Created by Omkar  on 1/16/26.
+//
+
+import Testing
+@testable import HomeUI
+
+struct HomeUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
