02107bb96805461650d42343eba779aa15286427
Omkar Ranawade <omkar.ranawade98@gmail.com>
2026-01-19 16:32:44 -0500

    Addition app is added addition function is added

--- HomeUI/ViewController.swift
+++ HomeUI/ViewController.swift
@@ -9,9 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstnumber: UITextField!
+    @IBOutlet weak var secondnumber: UITextField!
+    @IBOutlet weak var showAnswer: UITextField!
+    
+    
+    @IBAction func totalSum(_ sender: Any) {
+            guard let text1 = firstnumber.text,let num1 = Int(text1),
+                  let text2 = secondnumber.text, let num2 = Int(text2)
+            else {
+                return
+            }
+            let sum = num1 + num2
+            showAnswer.text = "\(sum)"
+    }
+    
+    @IBAction func clearDataAction( sender: Any ) {
+        showAnswer.text = ""
+        firstnumber.text = ""
+        secondnumber.text = ""
+    }
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        showAnswer.text = ""
     }
 
 
